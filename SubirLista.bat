5b88c57165979aca7754475064800ddff00740a6
Diego Shalom <diegoshalom@gmail.com>
2021-07-29 14:39:57 -0300

    test

--- SubirLista.bat
+++ SubirLista.bat
@@ -4,18 +4,17 @@ cd C:\REPOS\DistribuidoraShalom
 copy lastlog.txt previouslog.txt /y
 date /t > lastlog.txt
 time /t >> lastlog.txt
-echo "Ejecuto python milista.py" >> lastlog.txt
+echo Ejecuto python milista.py >> lastlog.txt
 python milista.py >> lastlog.txt
-echo "Ejecuto git pull" >> lastlog.txt
-echo Escribí "zapato" 
+echo Ejecuto git pull >> lastlog.txt
+echo Escribir "zapato" 
 git pull >> lastlog.txt
-echo "Ejecuto git pull" >> lastlog.txt
-echo "Ejecuto git add ."
+echo Ejecuto git add .
 git add . >> lastlog.txt
-echo "Ejecuto git commit" >> lastlog.txt
+echo Ejecuto git commit >> lastlog.txt
 git commit -m "test" >> lastlog.txt
-echo "Ejecuto git push" >> lastlog.txt
-echo Escribí "zapato" 
+echo Ejecuto git push >> lastlog.txt
+echo Escribir "zapato" 
 git push >> lastlog.txt 
-echo "Listo"  >> lastlog.txt
+echo Listo  >> lastlog.txt
 time /t >> lastlog.txt
