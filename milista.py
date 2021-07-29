c76fa55de36380bf3d165042106216ce65321560
diegoshalom <diegoshalom@gmail.com>
2021-07-29 12:48:23 -0300

    Segundo commit

--- milista.py
+++ milista.py
@@ -4,18 +4,17 @@ Created on Wed Jul 28 10:36:56 2021
 
 @author: Diego
 """
+#%% Cargo página template
 
-fname = 'e:/repos/listanegocio/TEXTO.TXT'
-
-fname = 'e:/repos/listanegocio/mypage.html'
+fname = 'mypagetemplate.html'
 with open(fname, 'r', encoding="utf-8") as f:
     miwebpage  = f.read()
 print(miwebpage  )
 
 
 
-#%%
-fname = 'e:/repos/listanegocio/TEXTO.TXT'
+#%% Cargo lista de precios
+fname = 'TEXTO.TXT'
 list_ = open(fname).read().split('\n')
 
 
@@ -42,8 +41,8 @@ for line in list_:
         # print(l)
         if len(l['desc'])==0: #si la descripción está vacía salteo
             continue
-        if l['unit']=='': #si el precio está vacía salteo
-            continue
+        # if l['unit']=='': #si el precio está vacía salteo
+            # continue
         
         L.append(l)
 print(L)
@@ -52,28 +51,27 @@ print(L)
 # 80.710000  Raso 0 FLUO x50m ....................... Pza   _  392.430
 # 80.723000  galon Pampa ............................ Pza   _  882.050
 
+#%% Convierto lista a string y reemplazo en el template
 tabla = ''
 for item in L:
-   itemlista = [item['cod'],item['desc'],item['unidad'],item['unit'],item['iva']]
-   tabla += '<tr>\n<td>'  + '</td>\n<td>'.join(itemlista) + '</td>\n<tr>'
+    if len(item['unit'])==0:
+        itemlista = ["",'<b>'+item['desc']+'</b>',"","",""]
+        tabla += '<tr>\n<td>'  + '</td>\n<td>'.join(itemlista) + '</td>\n<tr>'
+    else:
+        itemlista = [item['cod'],item['desc'],item['unidad'],item['unit'],item['iva']]
+        tabla += '<tr>\n<td>'  + '</td>\n<td>'.join(itemlista) + '</td>\n<tr>'
    
 
 out = miwebpage.replace('%TABLA%', tabla)
 out = out.replace('%FECHA%', fecha)
     
-fname = 'out.html'
+fname = 'docs/index.html'
 with open(fname, 'w', encoding="utf-8") as f:
     f.write(out)
 
 
 
-#    <tr>
-  #   <td>Koniglich Essen</td>
-  #   <td>Germany</td>
-  #   <td>Alfreds Futterkiste</td>
-  #   <td>Germany</td>
-  # </tr>
-  # nii_t1_1mm_filename = '/'.join(st_t1.split('/')[-5:]).replace('T1', 'T1_1mm')
+
 
 
     
