8ace273faa3755f66c5a2d760a534bd43e738f56
Diego Shalom <diegoshalom@gmail.com>
2025-11-08 09:35:41 -0300

    test

--- SubirLista.bat
+++ SubirLista.bat
@@ -4,7 +4,7 @@ copy c:\qb45\TEXTO.TXT . /y
 date /t > lastlog.txt
 time /t >> lastlog.txt
 echo Ejecuto python milista.py >> lastlog.txt
-path = C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Users\Elias y Pochi\AppData\Local\Programs\Python\Launcher\;C:\Users\Elias y Pochi\AppData\Local\Microsoft\WindowsApps;C:\Users\Elias y Pochi\AppData\Local\Programs\Python\Python313-32\
+path = C:\Users\Elias y Pochi\AppData\Local\Programs\Python\Python313-32\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Users\Elias y Pochi\AppData\Local\Programs\Python\Launcher\;C:\Users\Elias y Pochi\AppData\Local\Microsoft\WindowsApps;
 python milista.py >> lastlog.txt
 echo Ejecuto git pull >> lastlog.txt
 git pull >> lastlog.txt
